393ea909b8263c5d10504de4727a5b264f00cc83
juliusbutkus <j.butkus.skouds@gmail.com>
2023-11-08 15:36:12 +0200

    type listas padarytas neisoka dropdown

--- gardensketchpro/gardenplaner/urls.py
+++ gardensketchpro/gardenplaner/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('plant_types/', views.PlantTypeListView.as_view(), name='plant_types'),
 ]
--- gardensketchpro/gardenplaner/views.py
+++ gardensketchpro/gardenplaner/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from typing import Any
+from datetime import date, timedelta
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator
+from django.db.models.query import QuerySet, Q
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from . import models
-from django.http import HttpRequest
 
 
 def index(request: HttpRequest):
@@ -17,3 +26,26 @@ def index(request: HttpRequest):
         'num_visits': num_visits,
     }
     return render(request, 'gardenplaner/index.html', context)
+
+class PlantTypeListView(generic.ListView):
+    model = models.Type
+    template_name = "gardenplaner/plant_types.html"
+    context_object_name = "type_list"
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["search"] = True
+        return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("query")
+        if query:
+            queryset = queryset.filter(
+                Q(name_en__icontains=query) | 
+                Q(name_lt__icontains=query)  
+            )
+        return queryset
+    
+    
