1db11932e3e7761d41dfa04065594cd592a99100
juliusbutkus <j.butkus.skouds@gmail.com>
2023-11-09 14:21:32 +0200

    vertimai pakurti

--- gardensketchpro/gardensketchpro/settings.py
+++ gardensketchpro/gardensketchpro/settings.py
@@ -109,6 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+    (LANGUAGE_CODE, 'English'),
+    ('lt', 'Lietuvių'),
+)
 
 TIME_ZONE = 'UTC'
 
